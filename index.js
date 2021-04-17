dcd988558194ac535565b9fd70fcc171911ebb6b
MHirano <michelle@hiranohomes.com>
2021-04-17 12:08:37 -0700

    inquirer propt intial

--- index.js
+++ index.js
@@ -6,3 +6,27 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
+//Inquirer prompts
+async function prompt{
+    let responseDone = "";
+
+    do{
+        try{
+            let response = await inquirer.prompt([
+                //name
+                {
+                    type:"input",
+                    name: "name",
+                    message: "What is the employee's name?",
+                    validate: function validateName(name){
+                        return name !== "";
+                    }
+                },
+                //id
+                {
+                    type:"input"
+                }
+            ])
+        }
+    }
+}
