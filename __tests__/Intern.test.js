6b028e0dc0073ab0cbc0d7fbd807db7bd00a700b
MHirano <michelle@hiranohomes.com>
2021-04-17 11:49:06 -0700

    add intern tests

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,19 @@
+const Intern = require('../lib/Intern');
+
+test ('adding Intern school', () => {
+    const testValue = "UCI";
+    const e = new Intern("Jenny", 1, "jenny@bp.com", testValue);
+    expect(e.school).toBe(testValue);
+});
+
+test("getRole() to return Intern", () => {
+    const testValue = "Intern";
+    const e = new Intern("Jenny", 1, "jenny@bp.com", testValue);
+    expect(e.getRole()).toBe(testValue);
+});
+
+test("getRole function", () => {
+    const testValue = "Engineer";
+    const e = new Intern("Jenny", 1, "jenny@bp.com", testValue);
+    expect(e.getSchool()).toBe(testValue);
+});
