8710478de399bb446bcf5780d0802433edea1799
MHirano <michelle@hiranohomes.com>
2021-04-16 23:57:17 -0700

    Engineer tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,19 @@
+const Engineer = require('../lib/Engineer');
+
+test ('setting up github via constructor', () => {
+    const testValue = "MichelleHirano";
+    const employee = new Engineer("Jimin", 1, "jimin@bts.com", testValue);
+    expect(employee.github).toBe(testValue);
+});
+
+test("getRole function", () => {
+    const testValue = "Engineer";
+    const employee = new Engineer("Jimin", 1, "jimin@bts.com", testValue);
+    expect(employee.getRole()).toBe(testValue);
+})
+
+test("Get gitHub", () => {
+    const testValue = "MichelleHirano";
+    const employee = new Engineer("Jimin", 1, "jimin@bts.com", testValue);
+    expect(employee.getGitHub()).toBe(testValue);
+})
