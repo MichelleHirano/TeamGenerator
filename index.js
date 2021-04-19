957d990361130c32015a44bffad1dd7d70157541
MHirano <michelle@hiranohomes.com>
2021-04-18 17:46:01 -0700

    main function

--- index.js
+++ index.js
@@ -6,27 +6,106 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
-//Inquirer prompts
-async function prompt{
+// Inquirer prompts to collect user data
+async function prompt() {
     let responseDone = "";
 
-    do{
-        try{
+    do {
+        try {
+            console.log("---------------------------------------------");
             let response = await inquirer.prompt([
-                //name
                 {
-                    type:"input",
+                    type: "input",
                     name: "name",
-                    message: "What is the employee's name?",
-                    validate: function validateName(name){
+                    message: "What is employee's name?",
+                    validate: function validateName(name) {
                         return name !== "";
                     }
                 },
-                //id
                 {
-                    type:"input"
+                    type: "input",
+                    name: "id",
+                    message: "Enter employee's ID: ",
+                    validate: function validateName(name) {
+                        return name !== "";
+                    }
+                },
+                {
+                    type: "input",
+                    name: "email",
+                    message: "Enter employee's email address: ",
+                    // validation using email-validator
+                    validate: function validateName(name) {
+                        return validator.validate(name);
+                    }
+                },
+                {
+                    type: "list",
+                    name: "role",
+                    message: "What is the employee's position: ",
+                    choices: [
+                        "Engineer",
+                        "Intern",
+                        "Manager"
+                    ]
                 }
-            ])
+            ]);
+
+            let response2 = ""
+
+            if (response.role === "Engineer") {
+                response2 = await inquirer.prompt([{
+                    type: "input",
+                    name: "x",
+                    message: "What is the employee's github username?: ",
+                    validate: function validateName(name){
+                        return name !== "";
+                    },
+                }, ]);
+
+                // add to team Arr
+                const engineer = new Engineer(response.name, response.id, response.email, response2.x);
+                teamArray.push(engineer);
+            
+            } else if (response.role === "Manager") {
+                response2 = await inquirer.prompt([{
+                    type: "input",
+                    name: "x",
+                    message: "What is the employee's office number?: ",
+                    validate: function validateName(name){
+                        return name !== "";
+                    },
+                }, ]);
+
+                // add to team Arr
+                const manager = new Manager(response.name, response.id, response.email, response2.x);
+                teamArray.push(manager);
+
+            } else if (response.role === "Intern") {
+                response2 = await inquirer.prompt([{
+                    type: "input",
+                    name: "x",
+                    message: "What school is employee attending: ",
+                    validate: function validateName(name){
+                        return name !== "";
+                    },
+                }, ]);
+
+                // add to team Arr
+                const intern = new Intern(response.name, response.id, response.email, response2.x);
+                teamArray.push(intern);
+            }
+        } catch (err) {
+            return console.log(err);
         }
-    }
+        responseDone = await inquirer.prompt([{
+            type: "list",
+            name: "finish",
+            message: "Do you want to continue?: ",
+            choices: [
+                "Yes",
+                "No"
+            ]
+        },]);
+    } while (responseDone.finish === "Yes");
 }
